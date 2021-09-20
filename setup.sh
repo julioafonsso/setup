f85e72d7be8ef99a54111f09ef5f0681319becd9
Julio Afonso <julioafonsso@gmail.com>
2021-09-21 00:20:28 +0100

    removendo do default

--- setup.sh
+++ setup.sh
@@ -3,12 +3,9 @@ sudo apt-get update
 sh ./curl.sh
 sh ./chrome.sh
 sh ./java.sh
-sh ./eclipse.sh
 sh ./maven.sh
 sh ./git.sh
 sh ./node.sh
-sh ./visualcode.sh
-sh ./extension_visualcode.sh
 sh ./docker.sh
 sh ./docker_compose.sh
 sh ./spotify.sh
