9a7cd250222e6dbd3d999ad6d9e70e2d2ed01429
Julio Afonso <julioafonsso@gmail.com>
2021-10-19 09:39:09 +0100

    yahoo

---
+++ yahoofinance/com/julioafonsso/investimentos/util.py
@@ -0,0 +1,52 @@
+
+def fimFuncao():
+    print("")
+    print("")
+    print("")
+
+    input("Digite qualquer valor para voltar ...")
+    print("")
+    print("")
+    print("")
+
+def selectMenu(valores):
+    print()
+    print()
+    print("..............................................................")
+
+    print(formataItemMenu(" Selecione a opção desejada"))
+
+    for key, value in valores.items():
+        textoMenu = "       " + str(key) + " - " + value
+        print(formataItemMenu(textoMenu))
+
+    print("..............................................................")
+    print()
+    print()
+
+    opcao = int(input("Selecione a opção desejada: "))
+    print()
+    print()
+
+
+    if(opcao not in valores.keys()):
+        print("Menu Invalido!")
+    else:
+        print("Opção Selecionada : ")
+        print("     " + valores[opcao])
+
+    print()
+    print()
+    return opcao
+
+
+def formataItemMenu(linha):
+    tamanhoMaximo=60
+    tamanhoLinha = len(linha)
+    linhaRetorno = "." + linha
+    for x in range(tamanhoLinha,tamanhoMaximo):
+        linhaRetorno += " "
+
+    return linhaRetorno + "."
+
+
