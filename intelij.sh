b7b485b7ac3ccc0ed48636f0126c53fcf26ec688
Julio Afonso <julioafonsso@gmail.com>
2021-10-25 23:37:20 +0100

    inclusao InteliJ

---
+++ intelij.sh
@@ -0,0 +1,10 @@
+## /bin/sh
+
+wget https://download-cdn.jetbrains.com/idea/ideaIC-2021.2.2.tar.gz
+
+tar xvf ideaIC-2021.2.2.tar.gz
+
+sudo mv idea-IC-212.5284.40 /opt/idea
+
+rm ideaIC-2021.2.2.tar.gz
+
